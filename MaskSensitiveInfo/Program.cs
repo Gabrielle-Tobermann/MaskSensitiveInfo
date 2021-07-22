53c43ce28f8e184718e08ae3d1a3e06cce16d3e2
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-07-21 19:32:26 -0500

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your SSN");
+            var userString = Console.ReadLine();
+            // .substring returns a chunk of a string. PadLeft will cover the substring with a unicode 
+            string result = userString.Substring(userString.Length - 4).PadLeft(userString.Length, '*');
+            Console.WriteLine(result);
+        }
+    }
+}
